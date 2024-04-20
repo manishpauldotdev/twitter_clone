ffe7c61bc1562d9639e751f74f6e2a29fadb3b18
Manish Paul <manishpaul.dev@gmail.com>
2024-04-20 18:11:20 +0530

    added theme settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:twitter_clone/theme/theme.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,8 +10,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
+    return MaterialApp(
+      theme: AppTheme.theme,
+      home: const Scaffold(
         body: Center(
           child: Text('Hello World!'),
         ),
