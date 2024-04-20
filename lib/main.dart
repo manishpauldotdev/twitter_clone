193940e733086b294ee986453ebfbf37f662c457
Manish Paul <manishpaul.dev@gmail.com>
2024-04-20 21:09:19 +0530

    added login view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:twitter_clone/features/auth/views/login_view.dart';
 import 'package:twitter_clone/theme/theme.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MainApp extends StatelessWidget {
       theme: AppTheme.theme,
       home: const Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: LoginView(),
         ),
       ),
     );
