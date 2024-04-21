a0dfc264be85f529e749a04055d53fca88235fca
Manish Paul <manishpaul.dev@gmail.com>
2024-04-21 08:10:29 +0530

    added auth api with appwrite funcations and bunch of core classes

---
+++ lib/apis/auth_api.dart
@@ -0,0 +1,41 @@
+import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/models.dart' as model;
+import 'package:fpdart/fpdart.dart';
+import 'package:twitter_clone/core/failure.dart';
+import 'package:twitter_clone/core/type_defs.dart';
+
+abstract class IAuthAPi {
+  FutureEither<model.User> signup({
+    required String email,
+    required String password,
+  });
+}
+
+class AuthApi implements IAuthAPi {
+  final Account _account;
+
+  AuthApi({required Account account}) : _account = account;
+
+  @override
+  FutureEither<model.User> signup({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final account = await _account.create(
+        userId: ID.unique(),
+        email: email,
+        password: password,
+      );
+      return right(account);
+    } on AppwriteException catch (e, stackTrace) {
+      return left(
+        Failure(e.message ?? 'Some unexpected error occured', stackTrace),
+      );
+    } catch (e, stackTrace) {
+      return left(
+        Failure(e.toString(), stackTrace),
+      );
+    }
+  }
+}
